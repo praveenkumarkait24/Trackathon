3350420d32fa39a8fcae55317ee52fc1b9dd712b
Praveen <praveenkumarka.it24@bitsathy.ac.in>
2026-08-13 10:21:39 +0530

    feat: enforce ownership checks on edit/delete/rounds/achievements and designate creator as team lead at top of team members list

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -83,7 +83,7 @@ export const Login: React.FC = () => {
           <div className="space-y-2">
             <label className="text-sm font-semibold text-gray-300">Email Address</label>
             <div className="relative">
-              <Mail className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500 pointer-events-none" />
+              <Mail className="absolute left-3 top-3.5 h-5 w-5 text-gray-500" />
               <input
                 type="email"
                 value={email}
@@ -103,7 +103,7 @@ export const Login: React.FC = () => {
               </Link>
             </div>
             <div className="relative">
-              <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500 pointer-events-none" />
+              <Lock className="absolute left-3 top-3.5 h-5 w-5 text-gray-500" />
               <input
                 type="password"
                 value={password}
@@ -140,11 +140,11 @@ export const Login: React.FC = () => {
             disabled={loading}
             className="w-full py-3 bg-white/5 hover:bg-white/10 text-gray-200 hover:text-white rounded-xl font-bold transition-all border border-cardBorder flex items-center justify-center space-x-2.5 disabled:opacity-50"
           >
-            <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
-              <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
-              <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.06H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.94l2.85-2.22c-.09-.63-.19-1.3-.19-2.63z" fill="#FBBC05"/>
-              <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z" fill="#EA4335"/>
+            <svg className="w-4 h-4" viewBox="0 0 24 24">
+              <path
+                fill="#EA4335"
+                d="M12.24 10.285V14.4h6.887c-.648 2.41-2.519 4.114-5.136 4.114-3.41 0-6.19-2.78-6.19-6.19 0-3.41 2.78-6.19 6.19-6.19 1.487 0 2.851.53 3.92 1.4l3.078-3.079C18.995 2.096 15.824 1 12.24 1 5.96 1 12 5.96 1 12s4.96 11 11.24 11c6.516 0 11.26-4.577 11.26-11.24 0-.712-.082-1.393-.24-2.085H12.24z"
+              />
             </svg>
             <span>Continue with Google</span>
           </button>
